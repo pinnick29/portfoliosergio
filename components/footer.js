29067eb52946d78bb432c83fcc1a7b977ff34edb
pinnick29 <al376536@uji.es>
2022-06-17 18:38:43 +0200

    changed footer

--- components/footer.js
+++ components/footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
       <div id='footer' className="bg-gray-800">
         <div className= ' sticky  max-w-3xl lg:max-w-5xl flex mx-auto text-gray-300  font-medium h-1/6 justify-center gap-x-8 py-4 '> 
           <div className=" justify-center items-center  md:flex gap-x-4">
-            <p className = 'flex justify-center cursor-pointer hover:drop-shadow-2xl hover:font-bold '>Sergio López Viñuela</p>
+            <p className = 'flex justify-center cursor-pointer '>Sergio López Viñuela</p>
             <div className="flex gap-x-4 my-2">
               <a href="https://www.linkedin.com/in/sergio-l%C3%B3pez-vi%C3%B1uela-251464213/" target='_blank' className=" border-2 rounded-full p-3 hover:bg-gray-600" >
                 <FaLinkedinIn size={20}/>
