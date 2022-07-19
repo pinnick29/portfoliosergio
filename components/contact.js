bdef45776c850766109ee0d8ae2943048b0a066b
pinnick29 <al376536@uji.es>
2022-07-19 21:30:12 +0200

    Fixed contact layout again...

--- components/contact.js
+++ components/contact.js
@@ -14,8 +14,8 @@ const Contact = () => {
       <div id='contact' className='flex justify-center items-center  h-screen ' >
             <div className='flex flex-col items-center gap-y-6 justify-center pt-10  border-gray-300 '>
                 <p className="text-xl">Hello, I m</p>
-                <p className='h-16 sm:h-16 md:h-18 lg:h-24 text-4xl sm:text-5xl lg:text-6xl text-transparent bg-clip-text bg-gradient-to-r from-sky-500 to to-lime-500'>Sergio López Viñuela</p>
-                <p className='tracking-widest text-2xl'>Front-end Developer</p>
+                <p className=' text-3xl lg:text-5xl lg:h-20 text-transparent bg-clip-text bg-gradient-to-r from-sky-500 to to-lime-500'>Sergio López Viñuela</p>
+                <p className='tracking-widest  md:text-xl text-lg'>Front-end Developer & 3D Artist</p>
                 <div className="flex justify-center items-center flex-col md:flex-row gap-6 cursor-pointer">                
                     <div className="flex h-12 justify-center items-center w-40 border-2 gap-x-2 hover:bg-gray-600 rounded-full">
                         <p>See Projects</p>
@@ -34,7 +34,7 @@ const Contact = () => {
                     </div>
                 </div>                
             </div>
-            <div className="hidden lg:flex justify-center items-center m-10">
+            <div className="hidden md:flex justify-center items-center md:gap-20 m-10">
                 { <Image
                     className="rounded-full"
                     loader={myLoader}
